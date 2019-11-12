173b5e45d5b849f3cea200846845ac79afa52ef7
Dilip <dgagre15@in.ibm.com>
2019-11-12 16:24:30 +0530

    commitRest

---
+++ MGB/Rest/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "mysql",
+  database: "spring"
+});
+
+
+/*con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});*/
+/**/ 
+
+
+
+app.get('/getposts', (req, res) => {
+con.connect(function(err) {
+  if (err) 
+  throw err;
+  console.log('MySql Connected...');
+  //Select all customers and return the result object:
+  con.query("SELECT * FROM testing", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.send(result);
+  });
+});
+});
+/*
+app.get('/getposts', (req, res) => {
+    
+    con.query("SELECT * FROM testing", function (err, results, fields) {
+        if(err) throw err;
+        console.log(results);
+        console.log(query);
+        res.send('Posts fetched...');
+    });
+});*/
+
+
+app.listen(7777,() => {
+    console.log("server is running on port 7777");
+});
+module.exports = app;
